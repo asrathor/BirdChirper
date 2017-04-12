c01160118614e340514ab3f3584a3cf752daaaba
jrcharlo <jrcharlo@buffalo.edu>
2017-04-12 10:25:02 -0400

    Update main.py
    rewritten to extract features using frames of audio

--- main.py
+++ main.py
@@ -5,248 +5,279 @@ from pydub import AudioSegment
 import wave
 import sys
 import librosa.display
-import pywt
+from scipy.fftpack import fft
+from scipy.fftpack import ifft
 import math
 
-def file_convert():
+def file_convert(file):
+    #sound = AudioSegment.from_mp3("Surfbird_00002.mp3")
+    #sound.export("Surfbird2", format="wav", parameters=["-ac","1"])
+    sound = AudioSegment.from_wav(file)
+    sound.export("Mono "+file, format="wav", parameters=["-ac","1"])
 
-    #sound = AudioSegment.from_mp3("Acorn_Woodpecker_00001.mp3")
-    #sound.export("Acorn_Woodpecker1",format="wav")
-    #sound = AudioSegment.from_mp3("Allen_Hummingbird_00001.mp3")
-    #sound.export("Allen_Hummingbird1", format="wav")
-    sound = AudioSegment.from_mp3("Red-tailed_Hawk_00001.mp3")
-    sound.export("RedTailed_HawkMono1", format="wav", parameters=["-ac","1"])
+def frameFeature(file):
+    y, sr = librosa.load(file)
+    signaltime = librosa.get_duration(y=y, sr=sr)
+    framelength = 0.004 #0.010 s = 10 ms
+    numframes = signaltime/framelength#100
+    framesize = int(len(y)/numframes)
 
-    return 0
+    print("signal time: ", signaltime, "s")
+#    print("signal time*sampling rate: ", signaltime*sr)
+#    print("num data points: ", len(y))
+#    print("number of frames: ", numframes)
+#    print("framesize: ", framesize)
 
-def plot_datapoints(filename_wav):
+    fty = []
+    for i in range(0,len(y),framesize):
+        fty.append(fft(y[i:i+framesize]))
 
-    #source = wave.open(filename_wav,'r')
-    #signal = source.readframes(-1)
-    #signal = np.fromstring(signal, 'Int16')
-    #fs = source.getframerate()
-    arr = np.array((0,1))
-    signal, sr = librosa.load(filename_wav)
-    ft = np.fft.fft(signal)
-    a = int(len(ft)/2)
-    ft = ft[0:a]
-    #spec_c=spectral_centroid(ft)
-    #spec_b=signal_bandwidth(spec_c,ft)
-    #spec_d=spectral_flatness(ft)
-    spec_d = framesplit(signal)
-    Time=np.linspace(0,len(signal)/sr,num=len(signal))
-    plt.figure(1)
+#    print("num. fty frames", len(fty))
+
+##### CALCULATE SPECTRAL CENTROID #####
+    sc = []
+    for i in range(0,len(fty),1):
+        sci = spectralCentroid(fty[i])
+        sc.append(sci)
+
+    scsum = 0
+    for i in range(0, len(sc)):
+        scsum = scsum + sc[i]
+    scavg = scsum/len(sc)
+    print("sc avg: ",scavg) # Average Spectral Centroid (totally useless? dependent upon # frames)
+    print("sc sum ",scsum) # Sum of Spectral Centroid Frames
+
+#    print("num. sc frames", len(sc))
+
+##### CALCULATE BANDWIDTH #####
+    bw = []
+    for i in range(0,len(fty),1):
+        bwi = bandwidth(fty[i], sc[i])
+        bw.append(bwi)
+
+    bwsum = 0
+    for i in range(0,len(bw)):
+        bwsum = bwsum + bw[i]
+    bwavg = bwsum/len(bw)
+    print("bw avg: ",bwavg) # this value seems useless
+    print("bw sum: ",bwsum)
+
+#    print("num. bw frames", len(bw))
+
+##### CALCULATE SPECTRAL ROLLOFF #####
+    srf = []
+    th = 0.92
+    for i in range (0, len(fty), 1):
+        l = int(len(fty[i])/2)
+        h = int(l/4)
+        srfi = h
+
+        fsumt = 0
+        for n in range(0, l+1, 1):
+            fsumt = fsumt + fty[i][n]
+        fsumh = 0
+        fsumth = fsumt*th
+        for n in range(0, l+1, 1):
+            if(fsumh < fsumth):
+                fsumh = fsumh + fty[i][n]
+            else:
+                if(n > srfi):
+                    srfi = n
+                break
+        srf.append(srfi)
+
+    srfsum = 0
+    for i in range(0,len(srf)):
+        srfsum = srfsum + srf[i]
+    srfavg = srfsum/len(srf)
+    print("srf avg: ",srfavg)
+    print("srf sum: ",srfsum) # this value seems useless
+
+#    print("num. srf frames", len(srf))
+
+##### CALCULATE BAND ENERGY RATIO #####
+    ber = []
+    for i in range (0, len(fty), 1):
+        l = int(len(fty[i])/2)
+        h = srfavg #int(l/4) #sr[i]
+        fsumt = 0
+        fsumh = 0
+        for n in range (0, l+1, 1):
+            fsumt = fsumt + fty[i][n]
+            if(n < h):
+                fsumh = fsumh + fty[i][n]
+        beri = abs(fsumh/fsumt) # should this be abs(fsumh/fsumt) ?
+        ber.append(beri)
+
+    bersum = 0
+    for i in range(0,len(ber)):
+        bersum = bersum + ber[i]
+    beravg = bersum/len(ber)
+    print("ber avg: ",beravg)
+    print("ber sum: ",bersum)
+
+#    print("num. ber frames", len(ber))
+
+##### CALCULATE DELTA SPECTRUM MAGNITUDE #####
+    dsm = []
+    for i in range(0, len(fty)-1, 1):
+        l = int(len(fty[i])/2)
+        dsmi = 0
+        for n in range (0, l+1, 1):
+            if(i+1 < len(fty)-1):
+                dsmi = dsmi + abs(abs(fty[i][n]) - abs(fty[i+1][n]))
+            else:
+                dsmi = dsmi + abs(fty[i][n])
+        dsm.append(dsmi)
+
+    dsmsum = 0
+    for i in range(0,len(dsm)):
+        dsmsum = dsmsum + dsm[i]
+    dsmavg = dsmsum/len(dsm)
+    print("dsm avg: ",dsmavg)
+    print("dsm sum: ",dsmsum)
+
+#    print("num. dsm frames", len(dsm))
+
+##### CALCULATE SPECTRAL FLATNESS #####
+    sf = []
+    for i in range (0, len(fty), 1):
+        l = int(len(fty[i]/2))
+        gm = 1
+        am = 0
+        for n in range (0, l, 1):
+            gm = gm * abs(fty[i][n])**(1/l)
+            am = am + abs(fty[i][n])
+        am = am/l
+        sfi = 10*math.log10(gm/am)
+        sf.append(sfi)
+
+    sfsum = 0
+    for i in range(0,len(sf)):
+        sfsum = sfsum + sf[i]
+    sfavg = sfsum/len(sf)
+    print("sf avg: ",sfavg)
+    print("sf sum: ",sfsum)
+
+#    print("num. sf frames", len(sf))
+
+##### CALCULATE SHORT TIME SIGNAL ENERGY #####
+#    stse = []
+#    for i in range (0, len(fty), 1):
+#        l = int(len(fty[i]/2)
+#        for n in range (0, l, 1):
+
+# not finished here
+
+##### CALCULATE ZERO CROSSING RATE #####
+    zc = 0
+    for i in range(0, len(y)-1, 1):
+        if(y[i] > 0 and y[i+1] < 0):
+            zc = zc + 1
+        elif(y[i] < 0 and y[i+1] > 0):
+            zc = zc + 1
+    zcr = zc/len(y)
+    print("zcr: ", zcr)
+
+##### CALCULATE MIN/MAX FREQUENCIES #####
+
+def melspec(file):
+    y, sr = librosa.load(file)
+    fty = fft(y)
+    fty = fty[:int(len(fty)/2)]
+    spec = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128)
+    log_s = librosa.logamplitude(spec, ref_power=np.max)
+
+    maxf = 0
+    minf = 128
+
+    for i in range(0, len(log_s), 1):
+        for j in range(0, len(log_s[i]), 1):
+#            if(log_s[i][j] == 0): print(i)
+            if(log_s[i][j] > -20 and i > maxf):
+                maxf = i
+            if(log_s[i][j] > -20 and i < minf):
+                minf = i
+    print("maxf: ", maxf)
+    print("minf: ", minf)
+    mels = [math.log(minf,2)*minf, math.log(maxf,2)*maxf]
+    freqs = librosa.mel_to_hz(mels)
+#    freqs = librosa.core.mel_frequencies(13)
+
+    print(freqs)
+
+
+    dfty = np.diff(abs(fty))
+#    print("minf: ",minf)
+    plt.figure()
     plt.subplot(2,1,1)
-    plt.plot(Time,signal)
+    plt.plot(abs(fty))
+    plt.grid()
     plt.subplot(2,1,2)
-    #plt.plot(Time,lines)
-    #plt.subplot(3,1,3)
-    plt.plot(abs(ft))
-    #plt.show()
-
-    return 0
-
-#Roll-off frequency
-
-def rolloff_frequency(filename_wav):
-
-    y, sr = librosa.load(filename_wav)
-    rolloff=librosa.feature.spectral_rolloff(y=y,sr=sr)
-    print(rolloff)
-    print("Size:",rolloff.size)
-    return 0
-
-def spectogram(filename_wav):
-
-    y, sr = librosa.load(filename_wav)
-    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128)
-    log_S = librosa.logamplitude(S, ref_power=np.max)
-    print(log_S.shape)
-    plt.figure(figsize=(12,4))
-    print(log_S[:,0].size)
-    new_arr= np.zeros(log_S.shape)
-    print(new_arr.shape)
-    for i in range(log_S[0,:].size):
-        arr = log_S[:,i]
-        arr_max = np.amax(arr)
-        index= arr.argmax(axis=0)
-        #print("index:",index)
-        thres = 1.2*arr_max
-        #print("Threshold:",thres)
-        #j=arr_max
-        for j in range (index,log_S[:,i].size,1):
-            #print("j",j)
-            if(thres<=log_S[j,i]):
-                #new_arr[j,i]=log_S[j,i]
-                new_arr[j,i]=1
-            else:
-                break
-        for k in range (index,0,-1):
-            #print("k",k)
-            if(thres<=log_S[k,i]):
-                #new_arr[j,i]=log_S[j,i]
-                new_arr[k,i]=1
-            else:
-                break
-
-    prom_arr= new_arr*log_S
-
-    for i in range(new_arr[0,:].size):
-        for j in range(new_arr[:,0].size):
-            if(new_arr[j,i]==0):
-                prom_arr[j,i]=-80
-
-    #print(log_S)
-    #print(new_arr)
-    plt.subplot(3,1,1)
-    librosa.display.specshow(log_S,sr=sr,x_axis='time',y_axis='mel')
-    plt.colorbar(format='%+02.0f dB')
-    plt.subplot(3,1,2)
-    librosa.display.specshow(new_arr, sr=sr, x_axis='time', y_axis='mel')
-    plt.colorbar(format='%+02.0f dB')
-    plt.subplot(3,1,3)
-    librosa.display.specshow(prom_arr, sr=sr, x_axis='time', y_axis='mel')
-    plt.colorbar(format='%+02.0f dB')
-
-    plt.tight_layout()
-
-    plt.show()
-    return 0
-
-def mfcc(filename_wav):
-
-    y, sr = librosa.load(filename_wav)
-    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=130)
-    log_S = librosa.logamplitude(S, ref_power=np.max)
-    mfc = librosa.feature.mfcc(S=log_S, n_mfcc=13)
-
-    delta_mfcc = librosa.feature.delta(mfc)
-    delta2_mfcc = librosa.feature.delta(mfc, order=2)
-
-    plt.figure(figsize=(12,6))
-
-    plt.subplot(3,1,1)
-    librosa.display.specshow(mfc)
-    plt.ylabel('MFCC')
-    plt.colorbar()
-
-    plt.subplot(3, 1, 2)
-    librosa.display.specshow(delta_mfcc)
-    plt.ylabel('MFCC-$\Delta$')
-    plt.colorbar()
-
-    plt.subplot(3, 1, 3)
-    librosa.display.specshow(delta2_mfcc, sr=sr, x_axis='time')
-    plt.ylabel('MFCC-$\Delta^2$')
-    plt.colorbar()
-
-    plt.tight_layout()
-
-    plt.show()
-    return 0
-
-
-def wavelet(filename):
-
-    cA, cD = pywt.dwt(filename,'haar')
-    plt.plot(cA,cD)
+    plt.plot(abs(dfty))
+#    plt.plot(abs(fty2[:int(len(fty)/2)]))
+    plt.figure()
+    librosa.display.specshow(log_s, sr=sr, x_axis='time', y_axis='mel')
+#    plt.plot(log_s)
+#    plt.plot(mels)
+#    plt.colorbar(format='%+02.0f dB')
+    plt.grid()
     plt.show()
 
-def spectral_centroid(ft):
-    sumone=0
-    sumtwo=0
-    for i in range(len(ft)):
-        sumone = sumone + abs(ft[i])**2
+def negSlope(f, i, c): #checks next 10 values and sees if they are all decreasing
+    ret = 1
+    val = f[i]
+    for n in range(i, i+c, 1):
+        if(f[i] <= val):
+            val = f[i]
+        else:
+            ret = 0
+            break
 
-    for j in range(len(ft)):
-        sumtwo = sumtwo + (abs(ft[j])**2)*j
-    sc = sumtwo/sumone
-    #print("sc",sc)
+    return ret
+
+def posSlope(f, i, c): #checks next 10 values and sees if they are all inccreasing
+    ret = 1
+    val = f[i]
+    for n in range(i, i+c, 1):
+        if(f[i] >= val):
+            val = f[i]
+        else:
+            ret = 0
+            break
+
+    return ret
+
+def spectralCentroid(fty):
+    l = int(len(fty)/2)
+    num = 0
+    den = 0
+    for z in range(0, l+1, 1):
+        num = num + z*((abs(fty[z]))**2)
+        den = den + abs(fty[z])**2
+    sc = num/den
     return sc
 
-def signal_bandwidth(sc,ft):
-    sumone=0
-    sumtwo=0
-    for i in range(len(ft)):
-        sumone = sumone + abs(ft[i])**2
-    for j in range(len(ft)):
-        sumtwo = sumtwo + (((j-sc)**2)*abs(ft[j]))
-    bw = (sumtwo/sumone)**(1/2)
-    #print("bw",bw)
+def bandwidth(fty, sc):
+    l = int(len(fty)/2)
+    num=0
+    den=0
+    for n in range(0, l+1, 1):
+        num = num + ((n-sc)**2) * abs(fty[n])
+        den = den + abs(fty[n])**2
+    bw = (num/den)**(1/2)
     return bw
 
-def rolloff_freq(ft):
-    pow = 0
-    sumone=0
-    rf=0
-    #print("max",max(abs(ft))**2)
-    for i in range(len(ft)):
-        pow = abs(ft[i])**2
-        rf = max(pow,sumone)
-        if(sumone<pow):
-            sumone=pow
-    #print("rf_pow",rf)
-    thres = 0.95*rf
+def main():
+    #file_convert()
+    for i in range(1,58):
+        #filepath = "American Kestrel/AK"+str(i)+".wav" #92
+        #filepath = "Blue Jays/Blue Jay "+str(i)+".wav" #151
+        #filepath = "Canada Goose/Canada Goose "+str(i)+".wav" #156
+        filepath = "Northern Cardinal/Northern Cardinal "+str(i)+".wav" #58
+        print(filepath)
+        frameFeature(filepath)
+        #melspec(filepath)
+    sys.exit(0)
 
-    for j in range(len(ft)):
-        if(thres==abs(ft[j])):
-            rf=abs(ft[j])
-            break
-    #print("rf",rf)
-    return rf
-
-def delta_spectrum(ft):
-    dsm=0
-    for i in range(1,len(ft)):
-        dsm = dsm + abs(ft[i-1])-abs(ft[i])
-
-    #print("dsm",dsm)
-    return dsm
-
-def spectral_flatness(ft):
-
-    gm = abs(ft[0])
-    am = 0
-    sf = 0
-    for i in range(1, len(ft)):
-
-        gm = gm * (abs(ft[i])**(1/len(ft)))
-
-    for j in range(len(ft)):
-        am = am + abs(ft[i])
-
-    amm = am/len(ft)
-    res = gm/amm
-    sf = 10 * math.log10((res))
-    #print("sf",sf)
-    return sf
-
-def framesplit(signal):
-    frames=0
-    samplesize = len(signal)
-    framesize = samplesize / 100
-    x=0
-    avg=0
-    while(frames<100):
-
-        sig = signal[int(x):int(framesize+x)]
-        ft = np.fft.fft(sig)
-        ftt = ft[0:int(len(ft/2))]
-        sc = spectral_centroid(ftt)
-        avg = avg+sc
-        print("sc",sc)
-        x = framesize+x
-        frames = frames+1
-
-    avgg=avg/100
-    print("avg",avgg)
-arr= np.array([])
-for k in range(1,2):
-    filepath = "Bird Calls/Blue Jays/Blue Jay "+ str(k)+".wav"
-    run = plot_datapoints(filepath)
-    #arr = np.append(arr,run)
-print(arr.shape)
-
-
-
-sys.exit(0)
+main()
