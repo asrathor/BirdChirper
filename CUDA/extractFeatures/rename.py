c6e788a3442929fb074d2e441cc3412ed7796b3e
Justin <jrcharlo@buffalo.edu>
2017-05-14 12:48:36 -0400

    renamed GWT files

--- CUDA/extractFeatures/rename.py
+++ CUDA/extractFeatures/rename.py
@@ -1,10 +1,10 @@
 from subprocess import call
 
 def main():
-    for i in range(1,156):
-        filepath = "CG/Canada Goose "+str(i)+".wav" #92
+    for i in range(1,64):
+        filepath = "GWT/gwt"+str(i)+".wav"
         print(filepath)
-        newfile = "CG/CG"+str(i)+".wav"
+        newfile = "GWT/GWT"+str(i)+".wav"
         call(["mv",filepath, newfile]);
 
 main()
